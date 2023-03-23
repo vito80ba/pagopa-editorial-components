006ce4cba6ffa6c57342383adabaa226aaf484f7
Michele Moio <michele.moio@protonmail.com>
2023-03-23 15:51:24 +0100

    feat(EDCO-14): BannerLink

---
+++ src/components/Ctas.tsx
@@ -0,0 +1,59 @@
+import Button, { type ButtonProps } from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { type StackProps } from '@mui/system';
+import { isValidElement } from 'react';
+import { type CommonProps } from 'types/components';
+
+interface CtaButtonProps extends Partial<ButtonProps> {
+  text: string;
+}
+
+type CtaButton = CtaButtonProps | JSX.Element;
+
+export interface CtaProps extends CommonProps {
+  ctaButtons?: CtaButton[];
+  reverse?: boolean;
+  sx?: StackProps['sx'];
+}
+
+const isButtonProps = (button: CtaButton): button is CtaButtonProps => {
+  return !isValidElement(button);
+};
+
+export const Ctas = ({ ctaButtons, theme, reverse, sx }: CtaProps) => {
+  const buttonsThemeDefault: ButtonProps[] = [
+    {
+      color: theme === 'dark' ? 'negative' : 'primary',
+      variant: 'contained',
+    },
+    {
+      color: theme === 'dark' ? 'negative' : 'primary',
+      variant: 'outlined',
+    },
+  ];
+
+  return ctaButtons?.length ? (
+    <Stack
+      display="flex"
+      direction={reverse ? 'row-reverse' : 'row'}
+      justifyContent="left"
+      gap={2}
+      sx={sx}
+    >
+      {ctaButtons.map((button: CtaButton, i) => {
+        return isButtonProps(button) ? (
+          <Button
+            sx={{ width: { md: 'auto', xs: '100%' }, display: 'flex' }}
+            key={`${button.text}-${i}`}
+            {...buttonsThemeDefault[i]}
+            {...button}
+          >
+            {button.text}
+          </Button>
+        ) : (
+          button
+        );
+      })}
+    </Stack>
+  ) : null;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { BannerLink } from 'components/BannerLink';
 import { Editorial } from 'components/Editorial';
 import Hero from 'components/Hero';
 
 export default {
+  BannerLink,
   Editorial,
   Hero,
 };
